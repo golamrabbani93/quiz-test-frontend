02fdb4d882e5a49a49ec4c7590efac40b017e179
Golam Rabbani <golamrabbani9373@gmail.com>
2025-08-09 16:00:53 +0600

    show error message in under input

--- src/components/form/QuizInput.tsx
+++ src/components/form/QuizInput.tsx
@@ -14,10 +14,13 @@ const QuizInput = ({type, name, label}: TInputProps) => {
 		<div style={{marginBottom: '20px'}}>
 			<Controller
 				name={name}
-				render={({field}) => (
-					<Form.Item label={label}>
-						<Input {...field} type={type} id={name} size="large" />
-					</Form.Item>
+				render={({field, fieldState: {error}}) => (
+					<>
+						<Form.Item label={label}>
+							<Input {...field} type={type} id={name} size="large" />
+							{error && <small style={{color: 'red'}}>{error.message}</small>}
+						</Form.Item>
+					</>
 				)}
 			/>
 		</div>
