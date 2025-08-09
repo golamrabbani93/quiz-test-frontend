a7cc220e5919dbe6bb318900687970e03d578fd0
Golam Rabbani <golamrabbani9373@gmail.com>
2025-08-09 21:48:08 +0600

    make conditional route for welcome page

--- src/components/ui/Welcome.tsx
+++ src/components/ui/Welcome.tsx
@@ -1,6 +1,9 @@
 import {Link} from 'react-router-dom';
+import {useAppSelector} from '../../redux/hooks';
+import {getCurrentUser} from '../../redux/features/auth/authSlice';
 
 const Welcome = () => {
+	const user = useAppSelector(getCurrentUser);
 	return (
 		<div className="min-h-screen bg-gradient-to-r from-blue-500 to-indigo-600 flex flex-col justify-center items-center px-6">
 			<h1 className="text-white text-4xl md:text-6xl font-extrabold mb-6 text-center">
@@ -11,18 +14,29 @@ const Welcome = () => {
 				your journey today!
 			</p>
 			<div className="flex flex-col sm:flex-row gap-4">
-				<Link
-					to="/login"
-					className="bg-transparent border-2 border-white text-white font-semibold py-3 px-8 rounded-md hover:bg-white hover:text-indigo-700 transition"
-				>
-					Login
-				</Link>
-				<Link
-					to="/register"
-					className="bg-indigo-700 text-white font-semibold py-3 px-8 rounded-md shadow-md hover:bg-indigo-800 transition"
-				>
-					Register
-				</Link>
+				{user?.role ? (
+					<Link
+						to={`/${user?.role}`}
+						className="bg-transparent border-2 border-white text-white font-semibold py-3 px-8 rounded-md hover:bg-white hover:text-indigo-700 transition"
+					>
+						Go to Dashboard
+					</Link>
+				) : (
+					<>
+						<Link
+							to="/login"
+							className="bg-transparent border-2 border-white text-white font-semibold py-3 px-8 rounded-md hover:bg-white hover:text-indigo-700 transition"
+						>
+							Login
+						</Link>
+						<Link
+							to="/register"
+							className="bg-indigo-700 text-white font-semibold py-3 px-8 rounded-md shadow-md hover:bg-indigo-800 transition"
+						>
+							Register
+						</Link>
+					</>
+				)}
 			</div>
 		</div>
 	);
