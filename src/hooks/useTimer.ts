e945280876d39c669080eaf5b1cf2fa9b9436e4f
Golam Rabbani <golamrabbani9373@gmail.com>
2025-08-10 12:41:41 +0600

    update time hooks

--- src/hooks/useTimer.ts
+++ src/hooks/useTimer.ts
@@ -1,17 +1,16 @@
 // hooks/useTimer.ts
 import {useState, useEffect} from 'react';
 
-export const useTimer = (initialSeconds: number, onExpire: () => void) => {
+export const useTimer = (initialSeconds: number) => {
 	const [secondsLeft, setSecondsLeft] = useState(initialSeconds);
 
 	useEffect(() => {
 		if (secondsLeft <= 0) {
-			onExpire();
 			return;
 		}
 		const timer = setTimeout(() => setSecondsLeft((s) => (s > 0 ? s - 1 : 0)), 1000);
 		return () => clearTimeout(timer);
-	}, [secondsLeft, onExpire]);
+	}, [secondsLeft]);
 
 	return {secondsLeft, setSecondsLeft};
 };
